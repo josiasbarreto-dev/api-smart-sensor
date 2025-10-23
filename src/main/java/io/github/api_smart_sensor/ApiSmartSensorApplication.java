3be0f676a4f8f267a6f72e3decbab91df16fd350
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-23 15:09:29 -0300

    first commit

---
+++ src/main/java/io/github/api_smart_sensor/ApiSmartSensorApplication.java
@@ -0,0 +1,13 @@
+package io.github.api_smart_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSmartSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSmartSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/api_smart_sensor/ApiSmartSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.api_smart_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSmartSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
